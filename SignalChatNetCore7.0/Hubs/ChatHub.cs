1d2ec5a08b82044bb846336edc4926c42bfc7c45
Ali Can Yücel <yucelalican30@gmail.com>
2023-09-29 16:01:52 +0300

    ChatAppNetcore7Mvc

---
+++ SignalChatNetCore7.0/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalChatNetCore7._0.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user,string message)
+        {
+            // clientları dinle
+            await Clients.All.SendAsync("mesal alındı",user,message);
+        }
+    }
+}
